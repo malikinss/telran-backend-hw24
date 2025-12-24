952289608312fe685554902dedca68336d97f0b1
Sam Malikin <workmalikin@gmail.com>
2025-12-24 17:50:38 +0200

    update tests/__init__py to include new dictionary tests
    * Added imports for `TestMyDict`, `TestMySortedDict`, and `TestDictCache`
    * Updated `__all__` to expose the new test classes for unified test discovery

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+# tests/__init__.py
+
+from .test_my_dict import TestMyDict
+from .test_my_sorted_dict import TestMySortedDict
+from .test_dict_cache import TestDictCache
+
+__all__ = ['TestMyDict', 'TestMySortedDict', 'TestDictCache']
